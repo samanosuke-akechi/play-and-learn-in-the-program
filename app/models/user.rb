de4d6a5d5b341fc362c6e4522c81b0980bc7cd4d
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-22 19:34:41 +0900

    バリデーション設定
    英数字混合のみ設定できる。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
 end
