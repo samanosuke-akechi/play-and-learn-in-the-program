000dfc7c592ded3e6ce79acb2cbc43e55ad2ab7a
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-23 16:14:06 +0900

    投稿保存処理、ストロングパラメーター
    バリデーションが問題ない場合に保存を行い、トップページに遷移する。
    バリデーションに問題がある場合は、投稿ページに留まる。
    ストロングパラメーターとして、user_id（ログインユーザー）を結合

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -3,5 +3,22 @@ class OutputsController < ApplicationController
   end
 
   def new
+    @output = Output.new
+  end
+
+  def create
+    @output = Output.new(output_params)
+    if @output.valid?
+      @output.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def output_params
+    params.require(:output).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
