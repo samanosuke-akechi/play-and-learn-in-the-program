36075bb39c0bd98f210df3f767512497f62a7f2f
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-24 19:14:31 +0900

    ヘッダーのアウトプットプルダウン設定
    ヘッダーのアウトプットのプルダウンを新しい順に5件表示するよう設定。
    トップページと一覧ページと詳細ページに反映。
    DBからの取得の処理をプライベートメソッドにまとめて、before_actionにセット。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def top
-    @outputs = Output.includes(:user).order("created_at DESC").limit(5)
+    @outputs_list = Output.includes(:user).order("created_at DESC").limit(5)
   end
 end
